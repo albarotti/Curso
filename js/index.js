307c30a418fd3d695cbe02dc2b1561df584eabf9
ghabryelalbarotti <ghabryel.albarotti@gmail.com>
2022-04-11 17:50:30 -0300

    primeiro js

---
+++ js/index.js
@@ -0,0 +1,5 @@
+alert("Bem vindo! A seguir  pediremos que informe alguns  dados");
+let nome = prompt("Informe seu nome por favor ") ;
+let age = prompt("Informe sua Idade ") ;
+let confirmeAge = confirm("Sua idade é mesmo "+age+"?")
+alert(nome +" "+age+" is "+confirmeAge);
